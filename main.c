123dab9eb0085aece970f5b01839800b49464216
coldcue <coldcue@gmail.com>
2012-09-14 20:38:23 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	double x1, x2, y1, y2;
+
+	printf("Ird be az elso pontot\n");
+	scanf("%lf,%lf", &x1, &y1);
+
+	printf("Ird be a masodik pontot\n");
+	scanf("%lf,%lf", &x2, &y2);
+
+	printf("d=%f", sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)));
+
+	return 0;
+}
