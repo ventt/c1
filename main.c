374e0947ab5d969df8613f01646ce79d09a38024
andrew <coldcue@gmail.com>
2012-09-14 21:44:22 +0200

    ok

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14159265359
-
+/* (0,x metszes) (y metszes,0) */
 int main() {
-	double d, h;
 
-	printf("írd be az átmérőt és a magasságot!\n");
-	scanf("%lf,%lf", &d, &h);
+	double a, b;
+	printf("Írd be az a-t és a b-t!\n");
+	scanf("%lf,%lf", &a, &b);
 
-	printf("%.1f liter festék\n",(d*d*PI)/4+d*PI*h);
+	if (a == 0) {
+		printf("Nem metszi az x tengelyt! (%.2f,0)", b);
+	} else {
+		printf("(0;%.2f) (%.2f,0)", -b / a, b);
+	}
+
 	return 0;
 }
